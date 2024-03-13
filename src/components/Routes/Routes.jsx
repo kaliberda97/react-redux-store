2874f9e28eed725c279e48ad53a3f2e7accd1f3a
kaliberda97 <kaliberdavictoria97@.gmail.com>
2024-03-13 14:57:20 +0100

    add components

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import  Home  from "../Home/Home";
 
-
-
-
-const AppRoutes = () => {
-    return (
+const AppRoutes = () => (
         <Routes>
-            <Route index element ={<Home/>} />
+            <Route index element={< Home />} />
         </Routes>
     );
-}
 
 export default  AppRoutes;
