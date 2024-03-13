22044d475b350f556d0bf4713fa89709cb7b1a7d
kaliberda97 <kaliberdavictoria97@.gmail.com>
2024-03-13 16:30:18 +0100

    add components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from '../../styles/Header.module.css'
+
+import { ROUTES } from '../../utils/routes';
+
+import LOGO from '../../images/logo.svg'
+import AVATAR from '../../images/avatar.jpg'
+
+
+
 
 const Header = () => {
-  return ( <div>Header</div>
+  return ( 
+  <div className={styles.header}>
+    <div className={styles.logo}>
+      <Link to={'ROUTES.HOME'}>
+        <img src={LOGO} alt="Stuff " />   
+      </Link>
+    </div>
+
+    <div className= {styles.info}>
+    <div className= {styles.user}>
+    <div className= {styles.avatar} style={{backgroundImage: `url(${AVATAR})`}}
+    /> 
+
+    <div className={styles.username}>Guest</div>
+    </div>
+
+    <form className={styles.form}>
+    <div className={styles.icon}>
+      <svg className='icon'>
+        <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`}/>
+      </svg>
+    </div>
+    <div className={styles.input}>
+      <input type='search' name='search'/>
+    </div>
+    </form>
+    </div>
+  </div>
   )
 }
 
